c3e8a5bba3afdf6347f5c087d89067aa167b356f
wangxuanhao <1005853010@qq.com>
2025-10-24 16:00:49 +0800

    新增：组件库入口文件 目前voral正常提示

---
+++ packages/test-ui/components.ts
@@ -0,0 +1,7 @@
+import { Plugin } from 'vue'
+import { FlIcon } from '@test-ui/components/icon'
+import { FlButton } from '@test-ui/components/button'
+import { FlInput } from '@test-ui/components/input'
+
+const plugins = [FlIcon, FlButton, FlInput] as Plugin[]
+export default plugins
